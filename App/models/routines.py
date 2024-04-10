2e6b63f4476f326aa7d538ec32a858511020ba0b
Kyel-25 <157904909+Kyel-25@users.noreply.github.com>
2024-04-10 14:03:54 -0400

    Added dataset and Routines model

---
+++ App/models/routines.py
@@ -0,0 +1,21 @@
+from App.database import db
+
+class Routines(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    desc = db.Column(db.String, nullable=True)
+    body_part = db.Column(db.String(120), nullable=False)
+    equipment = db.Column(db.String(120), nullable=False)
+    level = db.Column(db.String(120), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    rating_desc = db.Column(db.String(120), nullable=False)
+
+    def __init__(self, title, desc, body_part, equipment, level, rating, rating_desc):
+        self.title = title
+        self.desc - desc
+        self.body_part = body_part
+        self.equipment = equipment
+        self.level = level
+        self.rating = rating
+        self.rating_desc = rating_desc
+        
