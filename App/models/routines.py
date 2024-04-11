b171e8afe1c7204b42e61a49a80db68b7757dab8
Kyle Lewis <kyle.lewis1@my.uwi.edu>
2024-04-11 00:37:25 +0000

    changed variables to unique=true

--- App/models/routines.py
+++ App/models/routines.py
@@ -3,16 +3,16 @@ from App.database import db
 class Routines(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
-    work_1 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=False)
-    work_2 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=False)
-    work_3 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=False)
-    work_4 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=False)
-    work_5 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=False)
-    work_6 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=False)
-    work_7 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=False)
-    work_8 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=False)
-    work_9 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=False)
-    work_10 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=False)
+    work_1 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=True)
+    work_2 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=True)
+    work_3 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=True)
+    work_4 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=True)
+    work_5 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=True)
+    work_6 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=True)
+    work_7 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=True)
+    work_8 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=True)
+    work_9 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=True)
+    work_10 = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=True, unique=True)
 
     def __init__(self, name, work_1, work_2, work_3, work_4, work_5, work_6, work_7, work_8, work_9, work_10):
         self.name = name
