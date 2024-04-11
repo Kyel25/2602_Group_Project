dfee2a945b81e061e5d8a8a4bb7aa1ba78294a70
Im-Sour <jordanboodoo@hotmail.com>
2024-04-11 01:19:41 +0000

    undo last

--- App/controllers/routine.py
+++ App/controllers/routine.py
@@ -1,5 +1,4 @@
 from App.models import workouts, routines
-from App.models.user import get_user_by_username
 from App.database import db
 
 def create_routine(name):
@@ -16,19 +15,4 @@ def get_routine_by_id(id):
         return None
 
 
-def create_routine_and_add_to_user(username, routine_name):
-    # Get the user by username
-    user = get_user_by_username(username)
-    if user:
-        # Create the routine
-        new_routine = create_routine(routine_name)
-        if new_routine:
-            # Associate the routine with the user
-            new_routine.user_id = user.id
-            db.session.commit()
-            return "Routine '{}' added to user '{}' successfully.".format(routine_name, username)
-        else:
-            return "Failed to create the routine."
-    else:
-        return "User '{}' not found.".format(username)
 
